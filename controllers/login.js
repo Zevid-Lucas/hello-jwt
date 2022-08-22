6c718c0707989221089edcaf6a6df37201df8bee
David Costa <zevid.dev@outlook.com>
2022-08-22 18:55:04 -0300

    ✨ feat:add validateBody for login, add middleware of login with jwt

--- controllers/login.js
+++ controllers/login.js
@@ -1,17 +1,26 @@
 const Joi = require('joi');
+const jwt = require('jsonwebtoken');
 
-const loginController = {
-  validateBody(body) {
-    Joi.object({
-      username: Joi.string().min(5).alphanum().required(),
-      password: Joi.string().min(5).required(),
-    }).validate(body);
-  },
+const { JWT_SECRET } = process.env;
+
+const validateBody = (body) => 
+  Joi.object({
+    username: Joi.string().min(5).alphanum().required(),
+    password: Joi.string().min(5).required(),
+  }).validate(body);
   
-  async login(req, res, next) {
-    const { error } = this.validateBody(req.body);
-    if (error) return next(error);
-  }, 
-};
+module.exports = async (req, res, next) => {
+  const { error } = validateBody(req.body);
+  if (error) return next(error);
+  
+  const payload = {
+    username: req.body.username,
+    admin: false,
+  };
 
-module.exports = loginController;
+  const token = jwt.sign(payload, JWT_SECRET, {
+    expiresIn: '1h',
+  });
+
+  res.status(200).json({ token });
+}; 
